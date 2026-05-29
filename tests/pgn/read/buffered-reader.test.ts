7488f7b68035279fe4c4613daf308a05ca514dab
Norbert Krajewski <norberttony@icloud.com>
2026-05-29 18:05:07 -0400

    buffered reader tests multiple chunk sizes to ensure weird edge cases are caught

--- tests/pgn/read/buffered-reader.test.ts
+++ tests/pgn/read/buffered-reader.test.ts
@@ -1,10 +1,22 @@
 import { testReader } from "./abstract-reader";
 import { BufferedReader } from "../../../src/pgn/read/buffered-reader";
 
-testReader("BufferedReader", (pathToFile: string) => {
-    return new Promise<BufferedReader>(async (res, _) => {
-        const reader = new BufferedReader(pathToFile, 2);
-        await reader.open();
-        res(reader);
-    });
-});
+testBufferedReader(2);
+testBufferedReader(3);
+testBufferedReader(4);
+
+function testBufferedReader(chunkSize: number): void {
+    testReader(
+        `BufferedReader chunkSize ${chunkSize}`,
+        async (pathToFile: string) => await createBufferedReader(pathToFile, 2),
+    );
+}
+
+async function createBufferedReader(
+    pathToFile: string,
+    chunkSize: number
+): Promise<BufferedReader> {
+    const reader = new BufferedReader(pathToFile, chunkSize);
+    await reader.open();
+    return reader;
+}
